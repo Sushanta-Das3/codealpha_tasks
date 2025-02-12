c5f4258ba5f099cad88d725c0dea6fcf0e08f029
Sushanta Das <susantadas59713@gmail.com>
2025-02-12 12:37:16 +0530

    Added Task1

---
+++ codealpha_tasks/fibonacci.py
@@ -0,0 +1,8 @@
+def fib_seq(n):
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[i-1] + fib_seq[i-2])
+    return fib_seq
+
+n=int(input("Enter the number of terms: "))
+print(*fib_seq(n))
